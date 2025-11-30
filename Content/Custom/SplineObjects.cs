aca78df4b2b5a704336aa328da3427a09d9eb87e
arunkapila <arunj.kapila@gmail.com>
2025-11-30 13:34:26 +0000

    2.25.2
    - Disabled culling on track splines

--- Content/Custom/SplineObjects.cs
+++ Content/Custom/SplineObjects.cs
@@ -163,6 +163,7 @@ public static class SplineObjects
 
             hasSetup = true;
             actualSpline = gameObject.AddComponent<HermiteSpline>();
+            actualSpline.preventCulling = true;
             actualSpline.controlPoints = points;
             actualSpline.InternalPoints = [];
             actualSpline.subdivisions = 25;
