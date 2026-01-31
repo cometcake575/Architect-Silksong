40aed9cb30708f28ad597cf5b48fab5a2529feab
Moss <computerintestines@proton.me>
2026-01-31 17:05:09 -0500

    Update SisterSplinter EnemyFixers.cs

--- Behaviour/Fixers/EnemyFixers.cs
+++ Behaviour/Fixers/EnemyFixers.cs
@@ -1713,7 +1713,7 @@ public static class EnemyFixers
         fsm.GetState("Reposition").DisableAction(3);
     }
 
-    public static void FixSisterSplinter(GameObject obj)
+        public static void FixSisterSplinter(GameObject obj)
     {
         var fsm = obj.LocateMyFSM("Control");
         
@@ -1749,6 +1749,9 @@ public static class EnemyFixers
         fsm.GetState("Idle").AddAction(AdjustAllPos, 0);
         fsm.GetState("Stun Hit").AddAction(AdjustYPos, 0);
         AdjustAllPos();
+
+        fsm.GetState("Spike Summon").AddAction(() => obj.BroadcastEvent("TrySpikes"), 0);
+        fsm.GetState("Duo Fight").AddAction(() => obj.BroadcastEvent("TrySummon"), 0);
 
         /*fsm.FsmVariables.FindFsmGameObject("Spikes Folder").Value = 
             Object.Instantiate(_splinterSpikes, obj.transform.position, obj.transform.rotation);*/
